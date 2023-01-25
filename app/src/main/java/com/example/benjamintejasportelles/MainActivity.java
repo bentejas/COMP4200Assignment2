14eab147245bfc0a789a542c606d7a0358b2a196
bentejas <57376142+bentejas@users.noreply.github.com>
2023-01-25 18:49:39 -0500

    Logcat messages and full functionality added

--- app/src/main/java/com/example/benjamintejasportelles/MainActivity.java
+++ app/src/main/java/com/example/benjamintejasportelles/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.KeyEvent;
@@ -18,20 +20,25 @@ public class MainActivity extends AppCompatActivity {
     Button submitNamesButton;
     public static String currentNames = "";
     EditText currentNameText;
+    CheckBox isBlack;
     private static Boolean firstName = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("TejasPortelles", "onCreate() method MainActivity");
         setContentView(R.layout.activity_main);
         viewNamesButton = (Button) findViewById(R.id.viewNamesButton);
         submitNamesButton = (Button) findViewById(R.id.submitButton);
         currentNameText = (EditText) findViewById(R.id.inputText);
+        isBlack = (CheckBox) findViewById(R.id.checkBox);
+        // listener for transferring to view names activity button
         viewNamesButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, ViewNamesActivity.class);
                 System.out.println(currentNames);
                 intent.putExtra("addedNames", currentNames);
+                intent.putExtra("isBlack", isBlack.isChecked());
                 startActivity(intent);
             }
         });
@@ -73,5 +80,42 @@ public class MainActivity extends AppCompatActivity {
             currentNames += ", " + name;
         }
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TejasPortelles", "onStart() method MainActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("TejasPortelles", "onStop() method MainActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TejasPortelles", "onResume() method MainActivity");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("TejasPortelles", "onRestart() method MainActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TejasPortelles", "onDestroy() method MainActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TejasPortelles", "onPause() method MainActivity");
     }
 }
--- app/src/main/java/com/example/benjamintejasportelles/ViewNamesActivity.java
+++ app/src/main/java/com/example/benjamintejasportelles/ViewNamesActivity.java
@@ -1,9 +1,12 @@
 package com.example.benjamintejasportelles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,14 +14,26 @@ import android.widget.TextView;
 public class ViewNamesActivity extends AppCompatActivity {
     Button returnToMainButton;
     TextView nameViewText;
+    Boolean isBlack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.d("TejasPortelles", "onCreate() method ViewNamesActivity");
         setContentView(R.layout.view_names);
         returnToMainButton = (Button) findViewById(R.id.returnButton);
         nameViewText = findViewById(R.id.textViewNames);
         nameViewText.append(getIntent().getStringExtra("addedNames"));
+        isBlack = getIntent().getBooleanExtra("isBlack", false);
+        if(isBlack){
+            ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout);
+            constraintLayout.setBackgroundColor(Color.BLACK);
+            TextView listTitle = findViewById(R.id.textView);
+            listTitle.setTextColor(Color.WHITE);
+            TextView listItems = findViewById(R.id.textViewNames);
+            listItems.setTextColor(Color.WHITE);
+        }
         returnToMainButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -26,5 +41,42 @@ public class ViewNamesActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TejasPortelles", "onStart() method ViewNamesActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("TejasPortelles", "onStop() method ViewNamesActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TejasPortelles", "onResume() method ViewNamesActivity");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("TejasPortelles", "onRestart() method ViewNamesActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TejasPortelles", "onDestroy() method ViewNamesActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TejasPortelles", "onPause() method ViewNamesActivity");
     }
 }
